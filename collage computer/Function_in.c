c8b8b71998c31b9e7e48650450cac6087d84966a
Himanshu <codebreaker8055@gmail.com>
2021-12-28 11:42:45 +0530

    Function-in-C

---
+++ collage computer/Function_in.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void function()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("It is function:- %d\n", i);
+    }
+}
+
+int main()
+{
+    function();
+    return 0;
+}
