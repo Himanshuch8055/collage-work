17096691e203679506c9e91622e98d8dda2d009f
Himanshu <codebreaker8055@gmail.com>
2022-01-07 23:17:04 +0330

    All-in-one-add

--- Problems/All-in-one.c
+++ Problems/All-in-one.c
@@ -2,35 +2,57 @@
 
 int main()
 {
-    int number, prime, fact;
+    int choose, prime, fact;
     printf("1. Prime Number\n");
     printf("2. Even Number\n");
     printf("3. odd Number\n");
     printf("4. Factorial of the Number\n");
     printf("5. Fabonica series\n");
     printf("6. Sum of Natural Number\n\n");
-    printf("Enter the number: ");
-    scanf("%d", &number);
+    printf("What opretion do you want: ");
+    scanf("%d", &choose);
 
-    switch (number)
+    switch (choose)
     {
     case 1:
-    printf("Enter the Number");
-    scanf("%d",&prime);
-    printf("Prime Numbers are: \n");
-    for(int i=1; i<=prime; i++)
-    {
-        fact=0;
-        for(int j=1; j<=prime; j++)
+        printf("Enter the Number: ");
+        scanf("%d", &prime);
+        printf("Prime Numbers are: ");
+        for (int i = 1; i <= prime; i++)
         {
-            if(i%j==0)
-                fact++;
+            fact = 0;
+            for (int j = 1; j <= prime; j++)
+            {
+                if (i % j == 0)
+                    fact++;
+            }
+            if (fact == 2)
+                printf("%d ", i);
         }
-        if(fact==2)
-            printf("%d " ,i);
-    }
+        break;
+
+    case 2:
+        printf("Enter the number: ");
+        scanf("%d", &prime);
+        printf("Even Number are: ");
+        for (int i = 0; i < prime; i++)
+        {
+            fact=0;
+            if (prime%2==0)
+            {
+                fact++;
+            }
+            if (fact == 2)
+            {
+                printf("%d", i);
+            }
+            
+            
+        }
+        
         break;
     default:
+        printf("NO OPRETION MATCH.");
         break;
     }
 
